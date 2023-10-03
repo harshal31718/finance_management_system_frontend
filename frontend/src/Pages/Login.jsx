0bfaaab98e4b25f6ffdcdc88c2319cd905e9d3b8
harshal31718 <harshaldodke31718@gmail.com>
2023-10-04 00:08:04 +0530

    created login page and CRU of user and data operation are complete

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Login = ({ logIn }) => {
+const defaultTheme = createTheme();
+
+// import Container from '@mui/material/Container';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Typography from '@mui/material/Typography';
+// import Link from '@mui/material/Link';
+// function Copyright(props) {
+//     return (
+//         <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//             {'Copyright © '}
+//             <Link color="inherit" href="">
+//                 FMS
+//             </Link>{' '}
+//             {new Date().getFullYear()}
+//             {'.'}
+//         </Typography>
+//     );
+// }
+
+export default function Login({ logIn }) {
+
     return (
-        <div className='container d-flex align-items-center'>
-            <div className='w-25 bg-primary'>
-                <h2>Log in</h2>
-                <button className='btn-primary' onClick={() => logIn()}>Log in with Google</button>
-            </div>
-            <div className='w-25 bg-secondary'>
-                <h2>Sign up</h2>
-                <button className='btn-primary' onClick={() => logIn()}>Sign up with Google</button>
-            </div>
-        </div>
-
-    )
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <Grid
+                sx={{
+                    backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '100vh',
+                    display: 'flex',
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: 'center',
+                }}
+            >
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: "25%",
+                    alignItems: "center"
+                }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Button
+                        onClick={() => logIn()}
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In With Google
+                    </Button>
+                </Box>
+            </Grid>
+        </ThemeProvider>
+    );
 }
-
-export default Login

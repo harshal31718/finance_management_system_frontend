5e7b43a619c7f9dec4154bd6a9c227b550ab3b49
harshal31718 <harshaldodke31718@gmail.com>
2023-10-17 17:48:46 +0530

    local categories and subcategories completed

--- frontend/src/Pages/Categories.jsx
+++ frontend/src/Pages/Categories.jsx
@@ -1,81 +1,96 @@
 import React, { useState } from 'react'
-import { Dropdown } from 'primereact/dropdown';
+import TextField from '@mui/material/TextField';
+
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
 
 const Categories = () => {
-    const [bar, setBar] = useState(false);
-    // const [categories, setCategories] = useState({ incomeCategories: [], expenseCategories: [] })
-    const [incomeCategories, setIncomeCategories] = useState(['job', "asset"]);
-    const [newCategory, setNewCategory] = useState("")
+    const [incomeCategories, setIncomeCategories] = useState([{ category: "job", subCategories: ["TCS"] }]);
+    const [newCategory, setNewCategory] = useState({ category: "", subCategories: [] });
+
     const [newSubCategory, setNewSubCategory] = useState({ category: "", subCategory: "" });
-    const [incomeSubCategories, setIncomeSubCategories] = useState([{ category: "job", subCategories: ["tcs"] }, { category: "asset", subCategories: ["stock", "realestate"] }])
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        if (name === "newCategory") {
-            setNewCategory(value);
-        }
-        else {
-            setNewSubCategory((prev) => {
-                return {
-                    ...prev,
-                    [name]: value
-                }
-            })
-        }
+    const handleCategory = (e) => {
+        setNewCategory((prev) => {
+            return {
+                ...prev,
+                category: e.target.value
+            }
+        })
     }
-
+    const AddCategory = (e) => {
+        setIncomeCategories((prev) => [...prev, newCategory]);
+        setNewCategory({ category: "", subCategories: [] });
+        e.preventDefault();
+    }
+    const handleSubCategory = (e) => {
+        const { name, value } = e.target;
+        setNewSubCategory((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    const AddSubCategory = (e) => {
+        setIncomeCategories((prev) => {
+            prev.map((element) => {
+                if (element.category === newSubCategory.category)
+                    element.subCategories.push(newSubCategory.subCategory);
+            })
+            return prev;
+        })
+        setNewSubCategory({ category: "", subCategory: "" });
+        e.preventDefault();
+    }
     return (
-        <div className='categories'>
-            <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                <button type="button" class={"btn" + (!bar ? " btn-primary" : " btn-outline-primary")} onClick={() => setBar(!bar)} >Income Categories</button>
-                <button type="button" class={"btn" + (bar ? " btn-primary" : " btn-outline-primary")} onClick={() => setBar(!bar)} >Expense Categories</button>
+        <div>
+            Categories
+            <div className='card m-1 p-2'>
+                <form onSubmit={AddCategory}>
+                    New Category
+                    <TextField className='' type="text" id="category" name='category' value={newCategory.category} label="New Category" onChange={handleCategory} size="small" required />
+                    <button className='btn btn-primary' type='submit'>Add</button>
+                </form>
             </div>
-            <div className="card" style={{ display: bar ? 'none' : '' }}>
-                <div>
-                    <form onSubmit={(e) => {
-                        const value = newCategory;
-                        setIncomeCategories((prev) => [...prev, value]);
-                        setNewCategory("");
-                        e.preventDefault();
-                    }}>
-                        <input type='text' name='newCategory' value={newCategory} onChange={handleChange} />
-                        <button type='submit'>newCategory</button>
-                    </form>
-                </div>
-                <div className='card'>
-                    <form onSubmit={(e) => {
-                        const value = newSubCategory;
-                        console.log(value);
-                        setIncomeSubCategories((prev) => {
-                            incomeSubCategories.find({ category: value.category }).exec().then((result) => {
-                                console.log(result);
-                            })
-                            return prev;
-                        })
-                        setNewSubCategory({ category: "", subCategory: "" });
-                        console.log(incomeSubCategories);
-                        e.preventDefault();
-                    }}>
-                        <Dropdown name='category' value={newSubCategory.category} onChange={handleChange} options={incomeCategories} placeholder="Select a Category" />
-                        <input type='text' name='subCategory' value={newSubCategory.subCategory} onChange={handleChange} />
-                        <button type='submit'>newSubCategory</button>
-                    </form>
-                </div>
-                <div>
-                    <h3>categories</h3>
-                    {incomeSubCategories.map((obj) =>
-                        <div>
-                            <h3>{obj.category}</h3>
-                            {/* {obj.subCategories.map((element) => <h6>{element}</h6>)} */}
-                        </div>
-                    )}
-                </div>
-                {/* <div>
-                            <h4>{obj.name}</h4>
-                            {obj.subCategories.map((element) => <h6>{element}</h6>)}
-                        </div> */}
+            <div className='card m-1 p-2'>
+                <form onSubmit={AddSubCategory}>
+                    New SubCategory
+                    <FormControl sx={{ minWidth: 120 }} size='small'>
+                        <InputLabel id="demo-simple-select-helper-label">Category</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            label="Category"
+                            name="category"
+                            value={newSubCategory.category}
+                            onChange={handleSubCategory}
+                            required
+                        >
+                            {incomeCategories.map(element =>
+                                <MenuItem value={element.category}>
+                                    {element.category}
+                                </MenuItem>
+                            )}
+                        </Select>
+                    </FormControl>
+                    <TextField className='' type="text" id="subCategory" name='subCategory' value={newSubCategory.subCategory} label="New SubCategory" onChange={handleSubCategory} size="small" required />
+                    <button className='btn btn-primary' type='submit'>Add</button>
+                </form>
             </div>
-
+            <div className='ccard m-1 p-2'>
+                {incomeCategories.map((category) =>
+                    <div>
+                        <h4>{category.category}</h4>
+                        {category.subCategories.map((subCategory) =>
+                            <h6>{subCategory}</h6>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }

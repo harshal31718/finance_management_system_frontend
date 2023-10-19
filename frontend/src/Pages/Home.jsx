4092a1f6a29519b258f22ab00cdf994ccd3f83ce
harshal31718 <harshaldodke31718@gmail.com>
2023-10-19 23:43:15 +0530

    V.0.3 checkpoint

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import List from '../components/List/List'
-import Overview from '../components/Overview/Overview'
 
 const Home = ({ profile, incomes, expenses, assets, liabilities }) => {
   return (
     <div className='home'>
-      <div className='container d-flex p-0'>
-        <div className='m-0 p-1'>
-          <h4>Dashboard</h4>
-          <h6>Name: {profile.name}</h6>
-          <h6>Email: {profile.email}</h6>
-        </div>
-        <Overview />
-      </div>
       <div className='container p-1 bg-primary bg-opacity-25' >
         <div className='row m-0 p-0'>
           <div className='col m-1 p-0' style={{ height: "30vh" }}>

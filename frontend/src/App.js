4c602cf251b7e466bf1674c398f0154caf93f022
harshal31718 <harshaldodke31718@gmail.com>
2023-10-21 15:47:05 +0530

    added sidebar and header on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
+import React, { useRef, useEffect, useState } from 'react';
+import { NavLink } from "react-router-dom";
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header'
+import Axios from 'axios';
 import Login from './Pages/Login';
 import Home from './Pages/Home'
 import Income from './Pages/Income';
@@ -11,6 +11,15 @@ import Assets from './Pages/Assets'
 import Liabilities from './Pages/Liabilities'
 import NewTransaction from './components/NewTransaction/NewTransaction';
 import Categories from './Pages/Categories';
+import { Avatar } from 'primereact/avatar';
+import { OverlayPanel } from "primereact/overlaypanel";
+import ViewListIcon from '@mui/icons-material/ViewList';
+import HomeIcon from '@mui/icons-material/Home';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import BusinessIcon from '@mui/icons-material/Business';
+import CategoryIcon from '@mui/icons-material/Category';
 
 const App = () => {
   // google useStates
@@ -23,6 +32,8 @@ const App = () => {
   const [liabilities, setLiabilities] = useState([{}]);
   const [incomeCategories, setIncomeCategories] = useState([{}]);
   const [expenseCategories, setExpenseCategories] = useState([{}]);
+  const [sideBarToggle, setSideBarToggle] = useState(false);
+  const profileOP = useRef(null);
 
   const logIn = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
@@ -144,20 +155,83 @@ const App = () => {
     <BrowserRouter>
       <div className='login'>
         {(user && profile) ? (
-          <>
-            <Header profile={profile} logOut={logOut} />
-            <div className='px-3' style={{ marginTop: "56px" }}>
-              <Routes>
-                <Route path='/' element={<Home profile={profile} incomes={incomes} expenses={expenses} assets={assets} liabilities={liabilities} />} />
-                <Route path='/income' element={<Income incomeData={incomes} incomeCategories={incomeCategories} addIncome={addIncome} editIncome={editIncome} deleteIncome={deleteIncome} />} />
-                <Route path='/expense' element={<Expense expenseData={expenses} expenseCategories={expenseCategories} addExpense={addExpense} editExpense={editExpense} deleteExpense={deleteExpense} />} />
-                <Route path='/assets' element={<Assets assetsData={assets} addAsset={addAsset} />} />
-                <Route path='/liabilities' element={<Liabilities liabilitiesData={liabilities} addLiability={addLiability} />} />
-                <Route path='/categories' element={<Categories incomeCategories={incomeCategories} expenseCategories={expenseCategories} addCategory={addCategory} addSubCategory={addSubCategory} />} />
-              </Routes>
+          <div className="layout" style={{ maxWidth: "100%" }}>
+            <div className="d-flex justify-content-between fixed-top border border-black m-0 p-2">
+              <div className="d-flex align-items-center">
+                <ViewListIcon fontSize='large' onClick={() => setSideBarToggle(!sideBarToggle)} />
+                <NavLink className="navbar-brand" to="/">FMS</NavLink>
+              </div>
+              <div className="d-flex align-items-top">
+                <Avatar icon="pi pi-user" shape="circle" onClick={(e) => profileOP.current.toggle(e)} style={{ backgroundColor: '#222222', color: '#ffffff', width: '40px', height: "40px" }} />
+                <OverlayPanel ref={profileOP}>
+                  <div className="">
+                    <div className="">
+                      <h6 className="m-0 p-0">Hello! {profile.name}</h6>
+                      <span>{profile.email}</span>
+                    </div>
+                    <div className="m-0 mt-2 p-0">
+                      <button className="btn btn-outline-danger" type="button" onClick={logOut} size="small" >Log Out</button>
+                    </div>
+                  </div>
+                </OverlayPanel>
+              </div>
+            </div>
+            <div className='d-flex' style={{ paddingTop: "56px", height: "100vh" }}>
+              <div className="border border-top-0 border-black m-0 px-2">
+                <ul className="navbar-nav mr-auto" style={{ display: sideBarToggle ? '' : 'none' }}>
+                  <li className="nav-item active">
+                    <NavLink className="nav-link d-flex align-items-center" to="/"><HomeIcon fontSize="large" /></NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/income"><AddShoppingCartIcon fontSize="large" /></NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/expense"><ShoppingCartCheckoutIcon fontSize="large" /></NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/assets"><BusinessIcon fontSize="large" /></NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/liabilities"><DirectionsCarIcon fontSize="large" /></NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/categories"><CategoryIcon fontSize="large" /></NavLink>
+                  </li>
+                </ul>
+                <ul className="navbar-nav mr-auto" style={{ display: !sideBarToggle ? '' : 'none' }}>
+                  <li className="nav-item active">
+                    <NavLink className="nav-link d-flex align-items-center" to="/"><HomeIcon fontSize="large" />Home</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/income"><AddShoppingCartIcon fontSize="large" />Income</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/expense"><ShoppingCartCheckoutIcon fontSize="large" />Expense</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/assets"><BusinessIcon fontSize="large" />Assets</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/liabilities"><DirectionsCarIcon fontSize="large" />Liabilities</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link d-flex align-items-center" to="/categories"><CategoryIcon fontSize="large" />Categories</NavLink>
+                  </li>
+                </ul>
+              </div>
+              <div className='flex-grow-1 overflow-auto m-0 p-1' styl>
+                <Routes>
+                  <Route path='/' element={<Home profile={profile} incomes={incomes} expenses={expenses} assets={assets} liabilities={liabilities} />} />
+                  <Route path='/income' element={<Income incomeData={incomes} incomeCategories={incomeCategories} addIncome={addIncome} editIncome={editIncome} deleteIncome={deleteIncome} />} />
+                  <Route path='/expense' element={<Expense expenseData={expenses} expenseCategories={expenseCategories} addExpense={addExpense} editExpense={editExpense} deleteExpense={deleteExpense} />} />
+                  <Route path='/assets' element={<Assets assetsData={assets} addAsset={addAsset} />} />
+                  <Route path='/liabilities' element={<Liabilities liabilitiesData={liabilities} addLiability={addLiability} />} />
+                  <Route path='/categories' element={<Categories incomeCategories={incomeCategories} expenseCategories={expenseCategories} addCategory={addCategory} addSubCategory={addSubCategory} />} />
+                </Routes>
+              </div>
             </div>
             <NewTransaction incomeCategories={incomeCategories} expenseCategories={expenseCategories} addUploadedData={addUploadedData} addIncome={addIncome} addExpense={addExpense} />
-          </>
+          </div>
         ) : (<Login logIn={logIn} />)}
       </div>
     </BrowserRouter>
